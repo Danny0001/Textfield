ab27056f66be998ca3df60502b52200dac8b1318
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-03-13 13:48:57 -0300

    Actualizacion de estilos y Adicion del logo

--- textfield/src/App.js
+++ textfield/src/App.js
@@ -10,8 +10,10 @@ class App extends Component {
     return (
       <div className="App">
         <form class="navbar-form" id="login">
+            <img className="logo" src='http://dragene.no/wp-content/uploads/2016/06/default1.jpg'/>
             <CampoDeTexto Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']}/>
             <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']}/>
+            <a href='http://www.google.com'className='ForgotPass'>Olvidaste tu contraseña?</a>
             <Boton Names={['Ingresar']}/>
           </form>
       </div>
