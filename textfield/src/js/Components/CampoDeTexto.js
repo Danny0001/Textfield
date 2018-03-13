c636de6adb63bd734e7047a13fad150ac4db0d9b
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-03-13 13:08:57 -0300

    Actualizacion de estilos

--- textfield/src/js/Components/CampoDeTexto.js
+++ textfield/src/js/Components/CampoDeTexto.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './../../css/App.css'
 
 class CampoDeTexto extends Component
 {
@@ -6,7 +7,7 @@ class CampoDeTexto extends Component
     const { Types, Names, PlaceHolders } = this.props
 
     return(
-      <div>
+      <div className='CampoDeTexto'>
         <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders}></input>
       </div>
     );
