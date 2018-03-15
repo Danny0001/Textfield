1a8d4993eac00dd2ac0818b15e3e6749847a0d3a
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-03-15 13:49:16 -0300

    Actualizacion Same

--- textfield/src/App.js
+++ textfield/src/App.js
@@ -13,21 +13,11 @@ class App extends Component {
         <form class="navbar-form" id="login">
             <img className="logo" src='http://dragene.no/wp-content/uploads/2016/06/default1.jpg'/>
             <TextoExplicativo Texto={['Correo']}/>
-<<<<<<< HEAD
+
             <CampoDeTexto id="email" Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']} required Patterns="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"/>
             <TextoExplicativo Texto={['Contraseña']}/>
             <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']} required Patterns="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"/>
-=======
-<<<<<<< HEAD
-            <CampoDeTexto Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']} Patterns={["[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"]} Titles={['Correo inválido']}/>
-            <TextoExplicativo Texto={['Contraseña']}/>
-            <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']} Patterns={["(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"]}/>
-=======
-            <CampoDeTexto id="email" Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']}  />
-            <TextoExplicativo Texto={['Contraseña']}/>
-            <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']} />
->>>>>>> a9aa209a774a94549f84ebfe4751ce8f5f2dcd27
->>>>>>> 530faf3008cd97538ac9e70725a7c1d52ad3cbbb
+
             <div className="link"><a href='http://www.google.com' className='ForgotPass'>Olvidaste tu contraseña?</a></div>
             <Boton Names={['Ingresar']}/>
           </form>
--- textfield/src/js/Components/CampoDeTexto.js
+++ textfield/src/js/Components/CampoDeTexto.js
@@ -10,21 +10,6 @@ class CampoDeTexto extends Component
     return(
       <div className='CampoDeTexto'>
         <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders} required pattern={Patterns}></input>
-=======
-<<<<<<< HEAD
-    const { Types, Names, PlaceHolders, Patterns, Titles } = this.props
-
-    return(
-      <div className='CampoDeTexto'>
-        <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders} pattern={Patterns} title={Titles}></input>
-=======
-    const { Types, Names, PlaceHolders, required, Pattern } = this.props
-
-    return(
-      <div className='CampoDeTexto'>
-        <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders} required pattern={Pattern}></input>
->>>>>>> a9aa209a774a94549f84ebfe4751ce8f5f2dcd27
->>>>>>> 530faf3008cd97538ac9e70725a7c1d52ad3cbbb
       </div>
     );
   }
