a9aa209a774a94549f84ebfe4751ce8f5f2dcd27
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-03-15 13:32:14 -0300

    convalid

--- textfield/src/App.js
+++ textfield/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CampoDeTexto from './js/Components/CampoDeTexto.js';
 import LineaInferior from './js/Components/LineaInferior.js';
 import TextoExplicativo from './js/Components/TextoExplicativo.js';
+import prub from './js/Components/prub.js';
 import Boton from './js/Components/Boton.js';
 import './css/App.css';
 
@@ -12,9 +13,9 @@ class App extends Component {
         <form class="navbar-form" id="login">
             <img className="logo" src='http://dragene.no/wp-content/uploads/2016/06/default1.jpg'/>
             <TextoExplicativo Texto={['Correo']}/>
-            <CampoDeTexto Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']} pattern={["[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"]}/>
+            <CampoDeTexto id="email" Names={['email']} PlaceHolders={['Ingresa tu correo']} Types={['email']}  />
             <TextoExplicativo Texto={['Contraseña']}/>
-            <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']} pattern={[".{6,}"]}/>
+            <CampoDeTexto Names={['password']} PlaceHolders={['Ingresa tu contraseña']} Types={['password']} />
             <div className="link"><a href='http://www.google.com' className='ForgotPass'>Olvidaste tu contraseña?</a></div>
             <Boton Names={['Ingresar']}/>
           </form>
--- textfield/src/js/Components/CampoDeTexto.js
+++ textfield/src/js/Components/CampoDeTexto.js
@@ -4,11 +4,11 @@ import './../../css/App.css'
 class CampoDeTexto extends Component
 {
   render(){
-    const { Types, Names, PlaceHolders, Pattern } = this.props
+    const { Types, Names, PlaceHolders, required, Pattern } = this.props
 
     return(
       <div className='CampoDeTexto'>
-        <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders} pattern={Pattern}></input>
+        <input type={Types} name={Names} class="form-control" placeholder={PlaceHolders} required pattern={Pattern}></input>
       </div>
     );
   }
