cf021cd65c3e26f564d4ae1cf68c634dc64b9e08
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-03-20 15:43:11 -0300

    Actualizacion: react-validation

--- textfield/src/js/Components/Login.js
+++ textfield/src/js/Components/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CampoDeTexto from './CampoDeTexto.js';
 import LineaInferior from './LineaInferior.js';
 import TextoExplicativo from './TextoExplicativo.js';
+import validator from 'validator';
 import prub from './prub.js';
 import Boton from './Boton.js';
 import './../../css/App.css';
@@ -26,11 +27,13 @@ class Login extends Component
       password: ''
     })
   }
-  
+
   handleSubmit(event) {
     console.log("We are awesome !!");
     this.clearAll();
     event.preventDefault();
+    //this.EmailValidator();
+    const { email, password} = this.state
   }
 
   handleChange(event){
@@ -38,13 +41,20 @@ class Login extends Component
     this.setState({[event.target.name]: event.target.value});
   }
 
+  /*EmailValidator(this.state.email){
+    const email=(this.state.email) =>{
+      if (!validator.isEmail(this.state.email)){
+        return '${this.state.email} is not a valid email.'
+      }
+    };
+  }*/
+
   render(){
     return(
       <div className="App">
         <form class="navbar-form" id="login" onSubmit={ this.handleSubmit }>
             <img className="logo" src='http://dragene.no/wp-content/uploads/2016/06/default1.jpg'/>
             <TextoExplicativo Texto={['Correo']}/>
-
             <CampoDeTexto
               id="email"
               value={this.state.email}
