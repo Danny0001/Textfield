7c6b89e14b7462feb91e1033277a87bcfa88852d
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-06-04 02:11:16 -0400

    Integrado el método para cerrar sesion

--- textfield/src/js/Components/Application.js
+++ textfield/src/js/Components/Application.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Boton from './Boton.js';
-
+import './../../css/App.css'
 class Application extends Component {
+
+constructor(props)
+{
+  super(props);
+  this.state = {
+    redirect:false,
+    logged:false,
+  };
+  this.function_logout = this.function_logout.bind(this);
+}
+function_logout(){
+  axios({
+      method: "POST",
+      url: "http://localhost:8001/api/user/logout",
+    })
+  .then(async response => {
+    this.setState({
+      logged:true,redirect:true
+    });
+    console.log("sesion cerrada correctamente")
+  })
+  .catch((err) => {
+      console.log("error al cerrar sesion")
+    })
+}
 
   render() {
     return (
       <div>
-        <Boton Names={['Logout']}/>
+        <button id="button" onClick={this.function_logout}>cerrar sesion</button>
         <img src="http://acidmonkey.net/img/pag_en_construccion.jpg"></img>
       </div>
     );
