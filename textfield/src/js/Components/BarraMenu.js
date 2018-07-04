95e70b39f72ccd1a6f9e2e4565fc12d379d217bd
Daniela Ramirez <Daniela.ramirez003@gmail.com>
2018-07-04 17:40:50 -0400

    comentarios posible infodisp

--- textfield/src/js/Components/BarraMenu.js
+++ textfield/src/js/Components/BarraMenu.js
@@ -4,13 +4,32 @@ import { IconNames } from "@blueprintjs/icons";
 
 import './../../css/App.css'
 class BarraMenu extends Component {
+/*
+  OpenAdd(){
+    var btn = document.getElementById('btnTools');
+   var className = btn.className;
+   var res = '';
+
+   if (className == "plus-button") {
+       className = "plus-button open";
+       document.getElementById('migration-form').className = "formcontainer show active";
+   }
+
+   else {
+       className = "plus-button";
+       document.getElementById('migration-form').className = "formcontainer";
+   }
+
+   btn.className = className;
+}
+*/
 
   render() {
     return (
       <div className="BarraMenu">
-        <button title="Agregar Dispositivo" type="button" class="pt-button pt-intent-success .buttonadd  ">
+        <button /*onClick={this.OpenAdd}*/ title="Agregar Dispositivo" type="button" class="pt-button pt-intent-success .buttonadd  ">
           Add
-          <span class=" pt-icon-standard pt-icon-add pt-align-right "></span>
+          <span class=" pt-icon-standard pt-icon-add pt-align-right"></span>
         </button>
       </div>
     );
