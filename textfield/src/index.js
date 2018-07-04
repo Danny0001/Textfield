2d3e0881dfbcbe868b5f7dfe8e14f29ef7088a2e
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-07-04 11:59:50 -0400

    Incorporacion Blueprint

--- textfield/src/index.js
+++ textfield/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Button, Intent, Spinner } from "@blueprintjs/core";
 import registerServiceWorker from './registerServiceWorker';
 /*
 import { express } from './config/config'
