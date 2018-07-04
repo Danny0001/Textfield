bca689291ad08526fc3c54de79d7d7de68820c30
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-07-04 11:17:30 -0400

    actualizacion

--- textfield/src/js/Components/CuadroDispositivo.js
+++ textfield/src/js/Components/CuadroDispositivo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import BotonControl from './BotonControl.js';
 import './../../css/App.css'
 class CuadroDispositivo extends Component {
 
   render() {
     return (
       <div className="CuadroDispositivo">
-        <div className="imagedispoitivo"></div>
+        <div className="imagedispoitivo">
+          <BotonControl></BotonControl>
+        </div>
         <div className="infodisp"></div>
       </div>
     );
