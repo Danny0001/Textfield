73d5569c002c9b7353ae3fa8d94a3a9f45302f56
Daniel Herrera Mena <daniel.herreram@utem.cl>
2018-08-10 17:22:10 -0400

    Profile

--- textfield/src/js/Components/EditProfile.js
+++ textfield/src/js/Components/EditProfile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './../../css/App.css'
-import { Link } from "react-router-dom";
 import NavBar from './NavBar.js'
 import LeftNavBar from './LeftNavBar.js'
 import { Button, Card, Elevation, Checkbox,Icon } from "@blueprintjs/core";
@@ -25,42 +24,11 @@ import {
 } from "@blueprintjs/core";
 import { Example, handleStringChange, IExampleProps } from "@blueprintjs/docs-theme";
 
-class Profile extends Component {
-  constructor(props)
-  {
-    super(props);
-    this.state={
-      USER:[{Name: 'NAME',
-      Lastname: 'LASTNAME',
-      Email:'EMAIL@EMAIL.cl',
-      Organization: 'ORGANIZATION'}]
-    }
-}
+class EditProfile extends Component {
+  
 cambio(){
   console.log("dfvsdgvsdvsdf");
 }
-
-ProfileUser(){
-  const renderProf = this.state.USER.map((usuario) =>{
-    return <div className="infoProfile ProfileTittles profiledata">
-        <div>
-          Name: {usuario.Name}
-        </div>
-        <div>
-          Lastname: {usuario.Lastname}
-        </div>
-        <div>
-          Email: {usuario.Email}
-        </div>
-        <div>
-          Organization: {usuario.Organization}
-        </div>
-  </div>
-
-})
-return renderProf;
-}
-
   render() {
     return (
       <div className="homeP">
@@ -70,9 +38,6 @@ return renderProf;
           <div className="cardProfile1">
             <div><h1>User Profile</h1></div>
             <ButtonGroup minimal={true} >
-                <Link to="/EditProfile" className="LinkEdit">
-                <Button class="bp3-button bp3-icon-add " type="button"  icon="edit">Edit</Button>
-                </Link>
                 <Button class="bp3-button bp3-icon-add " type="button"  icon="key">Change Password</Button>
                 {/* <Button class="bp3-button bp3-icon-add " type="button" intent="danger" icon="trash">Delete</Button>*/}
               {/*  <AnchorButton rightIcon="caret-down">Options</AnchorButton>*/}
@@ -82,7 +47,33 @@ return renderProf;
               <div className="imgProfileBox">
                 <img className="imgProfile" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"></img>
               </div>
-              {this.ProfileUser()}
+              <div className="infoProfile ProfileTittles">
+                <FormGroup
+                  label="Name"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput" id="text-input" placeholder="Ingresar Nombre" />
+                </FormGroup>
+                <FormGroup
+                  label="Lastname"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Ingresar Apellidos" />
+                </FormGroup>
+                <FormGroup
+                  label="E-mail"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Placeholder text" />
+                </FormGroup>
+                <FormGroup
+                  label="Organization"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Ingresar Apellidos" />
+                </FormGroup>
+                <Button type="button" class="bp3-button bp3-icon-add " intent="success" >Change</Button>
+            </div>
             <div>
             </div>
             </Card>
@@ -92,4 +83,4 @@ return renderProf;
   }
 }
 
-export default Profile;
+export default EditProfile;
