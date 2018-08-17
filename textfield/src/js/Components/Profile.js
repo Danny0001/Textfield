bb1d09adaa9b2b3fb147169f9d5a53d9db0ed380
Daniel Andres Herrera Mena <the_danny-the_new_revolution_of_the_music@live.cl>
2018-08-17 18:03:28 -0300

    Changes in profile

--- textfield/src/js/Components/Profile.js
+++ textfield/src/js/Components/Profile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './../../css/App.css'
-import { Link } from "react-router-dom";
 import NavBar from './NavBar.js'
 import LeftNavBar from './LeftNavBar.js'
 import { Button, Card, Elevation, Checkbox,Icon } from "@blueprintjs/core";
@@ -26,53 +25,39 @@ import {
 import { Example, handleStringChange, IExampleProps } from "@blueprintjs/docs-theme";
 
 class Profile extends Component {
+
   constructor(props)
   {
     super(props);
     this.state={
-      USER:[{Name: 'NAME',
+      Name: 'NAME',
       Lastname: 'LASTNAME',
       Email:'EMAIL@EMAIL.cl',
-      Organization: 'ORGANIZATION'}]
-    }
+      Organization: 'ORGANIZATION',
+      edit:false,
+    };
+
+    this.editItem= this.editItem.bind(this);
+}
+
+editItem(edit){
+  this.setState({edit: !this.state.edit});
+  console.log(this.state.edit)
 }
 cambio(){
   console.log("dfvsdgvsdvsdf");
 }
-
-ProfileUser(){
-  const renderProf = this.state.USER.map((usuario) =>{
-    return <div className="infoProfile ProfileTittles profiledata">
-        <div>
-          Name: {usuario.Name}
-        </div>
-        <div>
-          Lastname: {usuario.Lastname}
-        </div>
-        <div>
-          Email: {usuario.Email}
-        </div>
-        <div>
-          Organization: {usuario.Organization}
-        </div>
-  </div>
-
-})
-return renderProf;
-}
-
   render() {
+    const {edit}=this.state
     return (
       <div className="homeP">
         <NavBar></NavBar>
         <LeftNavBar></LeftNavBar>
-        <div className="content">
+        {(edit) ? (
+          <div className="content">
           <div className="cardProfile1">
             <div><h1>User Profile</h1></div>
             <ButtonGroup minimal={true} >
-                <Link to="/EditProfile" className="LinkEdit">
-                <Button class="bp3-button bp3-icon-add " type="button"  icon="edit">Edit</Button>
-                </Link>
                 <Button class="bp3-button bp3-icon-add " type="button"  icon="key">Change Password</Button>
                 {/* <Button class="bp3-button bp3-icon-add " type="button" intent="danger" icon="trash">Delete</Button>*/}
               {/*  <AnchorButton rightIcon="caret-down">Options</AnchorButton>*/}
@@ -82,11 +67,61 @@ return renderProf;
               <div className="imgProfileBox">
                 <img className="imgProfile" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"></img>
               </div>
-              {this.ProfileUser()}
+              <div className="infoProfile ProfileTittles">
+                <FormGroup
+                  label="Name"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput" id="text-input" placeholder="Ingresar Nombre" defaultValue={this.state.Name}/>
+                </FormGroup>
+                <FormGroup
+                  label="Lastname"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Ingresar Apellidos" defaultValue={this.state.Lastname}/>
+                </FormGroup>
+                <FormGroup
+                  label="E-mail"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Ingresar Email" defaultValue={this.state.Email}/>
+                </FormGroup>
+                <FormGroup
+                  label="Organization"
+                  labelFor="text-input"
+                  labelInfo="(required)">
+                  <InputGroup className="ProfileInput"  id="text-input" placeholder="Ingresar Organizacion" defaultValue={this.state.Organization}/>
+                </FormGroup>
+                <Button type="button" class="bp3-button bp3-icon-add " intent="success" onClick={this.editItem.bind(this)} >Change</Button>
+            </div>
             <div>
             </div>
             </Card>
+        </div>):
+      (<div className="content">
+      <div className="cardProfile1">
+        <div><h1>User Profile</h1></div>
+        <ButtonGroup minimal={true} >
+            <Button class="bp3-button bp3-icon-add " type="button"  icon="edit" onClick={this.editItem.bind(this)}>Edit</Button>
+            <Button class="bp3-button bp3-icon-add " type="button"  icon="key">Change Password</Button>
+            {/* <Button class="bp3-button bp3-icon-add " type="button" intent="danger" icon="trash">Delete</Button>*/}
+          {/*  <AnchorButton rightIcon="caret-down">Options</AnchorButton>*/}
+        </ButtonGroup>
+      </div>
+        <Card interactive={true} elevation={Elevation.TWO} className="cardProfile">
+          <div className="imgProfileBox">
+            <img className="imgProfile" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"></img>
+          </div>
+          <div className="infoProfile ProfileTittles profiledata">
+            <div>Name: {this.state.Name}</div>
+            <div>Lastname: {this.state.Lastname}</div>
+            <div>Email: {this.state.Email}</div>
+            <div>Organization {this.state.Organization}</div>
         </div>
+        <div>
+        </div>
+        </Card>
+    </div>)}
       </div>
     );
   }
